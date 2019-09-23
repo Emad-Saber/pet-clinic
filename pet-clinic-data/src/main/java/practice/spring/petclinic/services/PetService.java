faa32895a5883b751c560ce56937bd5e19ee8f02
Emad-Saber <30441542+Emad-Saber@users.noreply.github.com>
2019-09-23 17:32:05 +0200

    refactor common crud operations.

--- pet-clinic-data/src/main/java/practice/spring/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/practice/spring/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package practice.spring.petclinic.services;
 
 import practice.spring.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long Id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/practice/spring/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/practice/spring/petclinic/services/VetService.java
@@ -2,13 +2,7 @@ package practice.spring.petclinic.services;
 
 import practice.spring.petclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
 
-    Vet findById(Long Id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
